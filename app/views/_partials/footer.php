7a9352730fdbf3a38c87e0de208c62fa1bc6a42e
Francisco <francisco@SupremoX>
2024-01-22 07:29:41 -0300

    Melhorias estruturais no projeto.

--- app/views/_partials/footer.php
+++ app/views/_partials/footer.php
@@ -1,9 +1,8 @@
 		</main><!--/ MAIN -->
-	<section> <!-- End content-->
+	</section> <!-- End content-->
+	<script src="/js/main.js"></script>
+	<script src="/js/jquery.min.js"></script>
+	<script src="/js/bootstrap/bootstrap.min.js"></script>
+	<script src="/js/script.js"></script>
 </body>
-
-<script src="/js/main.js"></script>
-<script src="/js/jquery.min.js"></script>
-<script src="/js/bootstrap/bootstrap.min.js"></script>
-<script src="/js/script.js"></script>
 </html>
--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,5 +1,5 @@
 <?php require ('_partials/header.php'); ?>
-<h1 class="<?= theme('text-white-75', 'text-dark') ?>">Página inicial</h1>
-<p class="<?= theme('text-white-75', 'text-dark') ?> home-paragraph">Bem-vindo! PHP MVC Framework é uma estrutura rápida de PHP MVC projetada para permitir a fácil criação de sites e aplicativos web poderosos. É inspirado no <a class="<?= theme('text-light', 'text-primary') ?>" href="https://laravel.com/">Laravel</a>, e foi projetado para ser fácil de usar.</p>
-<img src="/img/logo.png" class="home-logo" >
+<h1 class="<?= theme('text-white-75', 'text-dark') ?> display-4">Página inicial</h1>
+<p class="<?= theme('text-white-75', 'text-dark') ?> lead home-paragraph">Bem-vindo! PHP MVC Framework é uma estrutura rápida de PHP MVC projetada para permitir a fácil criação de sites e aplicativos web poderosos. É inspirado no <a class="<?= theme('text-light', 'text-primary') ?>" href="https://laravel.com/">Laravel</a>, e foi projetado para ser fácil de usar.</p>
+<img src="/img/logo.png" class="home-logo" alt="Logo do Zay - Framework">
 <?php require ('_partials/footer.php'); ?>
